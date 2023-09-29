716e13a299b9d4ee5dd5c39fb00f7106746ea5b2
Chara Drimiskianaki <charadrim@yahoo.gr>
2023-09-29 19:26:23 +0300

    Update

--- app/page.js
+++ app/page.js
@@ -1,13 +1,17 @@
-'use client';
-
 import Image from 'next/image';
-import GenerateButton from './GenerateButton';
+import eyeCream from '../public/eye-cream.jpeg';
+import faceWash from '../public/face-wash.jpeg';
+import moisturizer from '../public/moisturizer-cream.jpeg';
+import retinol from '../public/retinol-cream.jpeg';
 
 export default function HomePage() {
   return (
     <main>
-      <h1>Beauty Products</h1>
-      <Image src="" alt="" width={200} />
+      <h1>Skincare Products</h1>
+      <Image src={faceWash} alt="face wash" width={300} height={300} />
+      <Image src={moisturizer} alt="moisturizer" width={300} height={300} />
+      <Image src={eyeCream} alt="eye cream" width={300} height={300} />
+      <Image src={retinol} alt="retinol" width={300} height={300} />
     </main>
   );
 }
